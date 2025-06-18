9d1be996932bc5f93b9f87c6b168255db836282e
penso996 <penso.andrea@outlook.it>
2025-06-18 13:29:41 +0200

    added fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  console.log(suggestions)
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setSuggestions([]);
+      return;
+    }
+    fetch(`http://localhost:3333/products?search=${query}`)
+      .then(res => res.json())
+      .then(data => setSuggestions(data))
+      .catch(error => console.error(error));
+  }, [query]);
 
   return (
     <>
@@ -13,6 +26,13 @@ function App() {
         value={query}
         onChange={e => setQuery(e.target.value)}
       />
+      {/* {suggestions.length > 0 && (
+        <div>
+          {suggestions.map((p, i) => (
+            <p key={i}>{p}</p>
+          ))}
+        </div>
+      )} */}
     </>
   )
 }
