4bc050087bd859da88d99cc6c75e2e9bb91e5e33
penso996 <penso.andrea@outlook.it>
2025-06-18 13:17:38 +0200

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function App() {
+
+  const [query, setQuery] = useState("");
 
   return (
     <>
+      <h1>Autocomplete</h1>
+      <input
+        type="text"
+        placeholder="Cerca"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
     </>
   )
 }
